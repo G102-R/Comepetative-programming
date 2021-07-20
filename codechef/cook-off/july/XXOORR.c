82f6f7e8f6fbea57a9618b9bbf91a2c3f30766c0
G102-R <84970399+G102-R@users.noreply.github.com>
2021-07-20 07:48:08 +0530

    Update XXOORR.c

--- codechef/cook-off/july/XXOORR.c
+++ codechef/cook-off/july/XXOORR.c
@@ -1,18 +1,46 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+#include<math.h>
 int main()
 {
     int T;
     scanf("%d",&T);
     while(T--)
     {
-        int n,k;
-        scanf("%d%d",&n,&k);
-        int A[n];
-        for(int i=0;i<n;i++)
+        int N,K;
+        scanf("%d%d",&N,&K);
+        int A[N],sum[33];
+        for(int i=0;i<33;i++)
+        {
+            sum[i]=0;
+        }
+        for(int i=0;i<N;i++)
         {
             scanf("%d",&A[i]);
         }
-    }   
+        for(int i=0;i<N;i++)
+        {
+            int x = A[i];
+            int j=32;
+            while(x>0)
+            {
+                sum[j]=sum[j]+x%2;
+                j--;
+                x=x/2;
+            }
+        }
+        int c=0;
+        for(int j=0;j<33;j++)
+        {
+            if(sum[j]%K==0)
+            {
+                c=c+sum[j]/K;
+            }
+            else{
+                c=c+1+sum[j]/K;
+            }
+        }
+        printf("%d\n",c);
+    } 
+    return 0 ;
 }
